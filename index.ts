a24c74ad09d59b8ff23abd726ae8868ff318609e
SukkaW <isukkaw@gmail.com>
2026-01-24 04:14:25 +0800

    chore: enable cdn cache, update build script

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
+import { fastStringArrayJoin } from 'foxts/fast-string-array-join';
 
 // Get document, or throw exception on error
 (() => {
@@ -7,6 +8,15 @@ import fs from 'fs';
     const doc = yaml.load(fs.readFileSync('./src/links.yml', 'utf-8'));
     fs.mkdirSync('./dist', { recursive: true });
     fs.writeFileSync('./dist/links.json', JSON.stringify(doc));
+
+    fs.cpSync('./src/img', './dist/img', { recursive: true });
+
+    fs.writeFileSync('./dist/_headers', fastStringArrayJoin([
+      '/links.json',
+      '  Cache-Control: public, max-age=300, stale-while-revalidate=60',
+      '/img/*',
+      '  Cache-Control: public, max-age=86400, stale-while-revalidate=3600',
+    ], '\n') + '\n');
   } catch (e) {
     console.error(e);
   }
